f12603984f18fd5dcbbdade77dc5ce8e6c702a0a
Luka Bostick <69057165+LukaBostick@users.noreply.github.com>
2024-02-11 08:04:09 -0600

    First commit

---
+++ index.php
@@ -0,0 +1,180 @@
+<?php require "includes/header.php"?>
+
+    <div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<div class="main-col">
+					<div class="block">
+						<h1 class="pull-left">Welcome to Virtual Voyage </h1>
+						<h4  class="pull-right"> S.A.A.S for Generating Education first repositories </h4>
+						<div class="divider"></div>
+						<div class="container">
+  <div class="row">
+    <div class="col">
+	<h3  class="pull-right"> Trending Topics </h3>
+    </div>
+    <div class="col">
+	<div class="d-flex justify-content-center align-items-center"> <!-- Added d-flex and justify-content-center align-items-center classes -->
+      <hr>
+  <div class="row row-cols-2">
+  <hr>
+    <div class="col">Thoughts on superconducting single flux quantum technology</div>
+	<hr>
+	<div class="container text-center">
+  <div class="row">
+    <div class="col">
+	<div class="card" style="width: 18rem;">
+        <img src="img/5.png" class="img" alt="">
+        <div class="card-body">
+          <h5 class="pull-left card-title "></h5> 
+		</div class="container text-center">
+    </div>
+    <div class="col ">
+	<a href="includes/discover.php" class="btn btn-primary align-btn-center">Go somewhere</a>
+    </div>
+  </div>
+  </div>
+  <hr>
+  <div class="col pull-left">AI incites and impact on Lives and Work</div>
+
+  <div class="container text-center">
+  <div class="row">
+    <div class="col">
+	<div class="card" style="width: 18rem;">
+        <img src="img/7.png" class="img" alt="">
+        <div class="card-body">
+          <h5 class="pull-left card-title "></h5> 
+		</div class="container text-center">
+    </div>
+    <div class="col">
+	<a href="includes/discover.php" class="btn btn-primary align-btn-center">Go somewhere</a>
+    </div>
+  </div>
+  </div>
+</div>
+  <div class="row">
+    <div class="col">
+	<hr>
+	<div class="divider"></div>
+    <div class="col">
+	<hr>
+    </div>
+  </div>
+</div> 
+      </div>
+    </div>
+    </div>
+</div>
+<div class="row">
+  <div class="col">
+    
+  </div>
+</div>
+<div class="row">
+  <div class="card-body">
+  </div>
+</div>
+</div>
+<li class="divider"></li>
+<button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" title="Tooltip on top">
+  Computer Science Education 
+</button>
+<button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="right" title="Tooltip on right">
+  Cybersecurity 
+</button>
+<button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Tooltip on bottom">
+  AI ML
+</button>
+<button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="left" title="Tooltip on left">
+  Programming
+</button>
+<button type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="left" title="Tooltip on left">
+  Software Engineering
+</button>
+<li class="divider"></li>
+						<hr>
+						<ul id="topics">
+							<li class="topic">
+							<div class="row">
+							<div class="col-md-2">
+								<img class="avatar pull-left" src="img/gravatar.png" />
+							</div>
+							<div class="col-md-10">
+								<div class="topic-content pull-right">
+									<h3><a href="topic.html">How did you learn CSS and HTML?</a></h3>
+									<div class="topic-info">
+										<a href="category.html">Development</a> >> <a href="profile.html">MOhamed Hassan</a> >> Posted on: June 12, 2014 
+										<span class="color badge pull-right">3</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="topic">
+						<div class="row">
+							<div class="col-md-2">
+								<img class="avatar pull-left" src="img/gravatar.png" />
+							</div>
+							<div class="col-md-10">
+								<div class="topic-content pull-right">
+									<h3><a href="topic.html">How to create new page dynamically in php</a> </h3>
+									<div class="topic-info">
+										<a href="category.html">Development</a> >> <a href="profile.html">MOhamed Hassan</a> >> Posted on: June 12, 2014 
+										<span class="color badge pull-right">7</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="topic">
+						<div class="row">
+							<div class="col-md-2">
+								<img class="avatar pull-left" src="img/gravatar.png" />
+							</div>
+							<div class="col-md-10">
+								<div class="topic-content pull-right">
+									<h3><a href="topic.html">Google Panda - Who's affected?</a></h3>
+									<div class="topic-info">
+										<a href="category.html">Search Engines</a> >> <a href="profile.html">MOhamed Hassan</a> >> Posted on: June 12, 2014 
+										<span class="color badge pull-right">4</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="topic">
+						<div class="row">
+							<div class="col-md-2">
+								<img class="avatar pull-left" src="img/gravatar.png" />
+							</div>
+							<div class="col-md-10">
+								<div class="topic-content pull-right">
+									<h3><a href="topic.html">Is Css3 is not working in IE8 and IE9?</a></h3>
+									<div class="topic-info">
+										<a href="category.html">Design</a> >> <a href="profile.html">MOhamed Hassan</a> >> Posted on: June 12, 2014 
+										<span class="color badge pull-right">2</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="topic">
+						<div class="row">
+							<div class="col-md-2">
+								<img class="avatar pull-left" src="img/gravatar.png" />
+							</div>
+							<div class="col-md-10">
+								<div class="topic-content pull-right">
+									<h3><a href="topic.html">Best Web Application Frameworks</a></h3>
+									<div class="topic-info">
+										<a href="category.html">Development</a> >> <a href="profile.html">MOhamed Hassan</a> >> Posted on: June 12, 2014 
+										<span class="color badge pull-right">4</span>
+									</div>
+								</div>
+							</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require "includes/footer.php"; ?>
